54023b46ae093d26aa8111b0083c84c63eefe3e0
adorda-hellosidecar <agustin.dorda@hellosidecar.com>
2023-02-18 20:55:19 -0300

    fix in dev environment

--- dev/main.tf
+++ dev/main.tf
@@ -7,3 +7,9 @@ module "database" {
   network_id = module.network.dev_network_id
   subnet_id = module.network.dev_subnet_id
 }
+
+module "runtime" {
+  source = "./runtime"
+  network_id = module.network.dev_network_id
+  subnet_id = module.network.dev_subnet_id
+}
