708bf7f3cf864286cb60f9753bf00c877ecab5ac
adorda-hellosidecar <agustin.dorda@hellosidecar.com>
2023-02-19 11:42:44 -0300

    fixes

--- dev/network/main.tf
+++ dev/network/main.tf
@@ -2,6 +2,7 @@ resource "aws_vpc" "network" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
+  enable_classiclink = true
 
   tags = {
     Name = "network"
