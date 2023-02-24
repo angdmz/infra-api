1c88a699f1d9d0bc134b3b4323e589668fe46206
angdmz <agustindorda@gmail.com>
2023-02-23 23:51:03 -0300

    fix in cidr block

--- dev/network/main.tf
+++ dev/network/main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "network" {
-  cidr_block = "10.0.0.0/24"
+  cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
 
@@ -11,7 +11,7 @@ resource "aws_vpc" "network" {
 
 resource "aws_subnet" "subnet" {
   vpc_id = aws_vpc.network.id
-  cidr_block = "10.0.0.0/22"
+  cidr_block = "10.0.1.0/24"
 
   tags = {
     Name = "subnet"
