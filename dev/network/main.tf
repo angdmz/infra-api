7a118b4f300197f3a9216a6353b644d180d6d869
angdmz <agustindorda@gmail.com>
2023-02-23 22:27:09 -0300

    new networking

--- dev/network/main.tf
+++ dev/network/main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "network" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = "10.0.0.0/24"
   enable_dns_hostnames = true
   enable_dns_support   = true
 
@@ -8,11 +8,82 @@ resource "aws_vpc" "network" {
   }
 }
 
+
 resource "aws_subnet" "subnet" {
   vpc_id = aws_vpc.network.id
   cidr_block = "10.0.1.0/24"
 
   tags = {
     Name = "subnet"
+  }
+}
+
+
+resource "aws_subnet" "pub_subnet" {
+  vpc_id                  = aws_vpc.network.id
+  cidr_block              = "10.1.0.0/22"
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.network.id
+}
+
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.network.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
+}
+
+resource "aws_route_table_association" "route_table_association" {
+  subnet_id      = aws_subnet.pub_subnet.id
+  route_table_id = aws_route_table.public.id
+}
+
+
+resource "aws_security_group" "ecs_sg" {
+  vpc_id      = aws_vpc.network.id
+
+  ingress {
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 65535
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "rds_sg" {
+  vpc_id      = aws_vpc.network.id
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = 3306
+    to_port         = 3306
+    cidr_blocks     = ["0.0.0.0/0"]
+    security_groups = [aws_security_group.ecs_sg.id]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 65535
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
   }
 }
