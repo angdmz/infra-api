c87c2003b22cbcaea519332b565b72b495f942a5
adorda-hellosidecar <agustin.dorda@hellosidecar.com>
2023-02-25 20:18:36 -0300

    fix in params

--- dev/main.tf
+++ dev/main.tf
@@ -5,7 +5,6 @@ module "network" {
 module "database" {
   source = "./database"
   network_id = module.network.dev_network_id
-  subnet_id = module.network.dev_subnet_az1_id
   runtime_security_id = module.runtime.runtime_security_id
   subnet_az1_id = module.network.dev_subnet_az1_id
   subnet_az2_id = module.network.dev_subnet_az2_id
