a68fcf09ec2c553a66ba9fd80702b046d9e51bc2
adorda-hellosidecar <agustin.dorda@hellosidecar.com>
2023-02-25 19:56:36 -0300

    fix in cidr block

--- dev/network/main.tf
+++ dev/network/main.tf
@@ -20,7 +20,7 @@ resource "aws_subnet" "subnet" {
 
 resource "aws_subnet" "rds_subnet" {
   vpc_id = aws_vpc.network.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-2b"
 }
 
