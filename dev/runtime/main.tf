0329052cc722863462333d129b849ff3461abf0d
adorda-hellosidecar <agustin.dorda@hellosidecar.com>
2023-02-26 22:08:13 -0300

    rollbacked iam roles

--- dev/runtime/main.tf
+++ dev/runtime/main.tf
@@ -39,20 +39,20 @@ data "aws_iam_policy_document" "ecs_agent" {
     }
   }
 }
-resource "aws_iam_role" "ecs_agent" {
-  name               = "ecs-agent"
-  assume_role_policy = data.aws_iam_policy_document.ecs_agent.json
+
+resource "aws_iam_role" "ecs_agent_role" {
+  name               = "ecs-agent-infra-api-dev"
+  assume_role_policy = data.aws_iam_policy_document.ecs_agent_data.json
 }
 
-resource "aws_iam_role_policy_attachment" "ecs_agent" {
-  role       = aws_iam_role.ecs_agent.name
+resource "aws_iam_role_policy_attachment" "ecs_agent_attachment" {
+  role       = aws_iam_role.ecs_agent_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
-
 resource "aws_iam_instance_profile" "ecs_agent" {
-  name = "ecs-agent"
-  role = aws_iam_role.ecs_agent.name
+  name = "ecs-agent-infra-api-dev"
+  role = aws_iam_role.ecs_agent_role.name
 }
 
 
